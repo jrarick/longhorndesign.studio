7776e436d9de96e188418a3952915d74a563ad2b
Joshua Rarick <rarick.joshua.e@gmail.com>
2025-01-01 21:26:36 -0600

    fixed og for real this time

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -1,6 +1,7 @@
 import { ImageResponse } from "next/og";
 import { loadGoogleFont } from "./load-google-font";
 import ldsLogo from "@/app/assets/lds-logo.png";
+import Image from "next/image";
 
 export async function GET(request: Request) {
   let url = new URL(request.url);
@@ -8,11 +9,20 @@ export async function GET(request: Request) {
 
   return new ImageResponse(
     (
-      <div tw="flex flex-col w-full h-full items-center justify-center bg-stone-950 text-marzipan-200 px-8">
-        <img src={ldsLogo.src} height={140} />
+      <div
+        tw="flex flex-col w-full h-full items-center justify-center px-8"
+        style={{
+          backgroundColor: "#0c0a09",
+        }}
+      >
         <div tw="flex flex-col w-full pt-12 md:items-center justify-between max-w-1/2">
-          <h2 tw="flex flex-col text-4xl tracking-tight">
-            It Could Be Time To Rethink Your Sticky Header
+          <h2
+            tw="flex flex-col text-4xl tracking-tight"
+            style={{
+              color: "#ffe09e",
+            }}
+          >
+            {title}
           </h2>
         </div>
       </div>
