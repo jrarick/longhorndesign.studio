98b778924a834fd34db9c82532ac14a1a90833a1
Joshua Rarick <rarick.joshua.e@gmail.com>
2025-01-01 20:57:32 -0600

    updated og generator

--- app/icon.tsx
+++ app/icon.tsx
@@ -1,4 +1,5 @@
 import { ImageResponse } from "next/og";
+import { loadGoogleFont } from "./og/load-google-font";
 
 // Image metadata
 export const size = {
@@ -6,25 +7,6 @@ export const size = {
   height: 32,
 };
 export const contentType = "image/png";
-
-async function loadGoogleFont(font: string, text: string) {
-  const url = `https://fonts.googleapis.com/css2?family=${font}&text=${encodeURIComponent(
-    text
-  )}`;
-  const css = await (await fetch(url)).text();
-  const resource = css.match(
-    /src: url\((.+)\) format\('(opentype|truetype)'\)/
-  );
-
-  if (resource) {
-    const response = await fetch(resource[1]);
-    if (response.status == 200) {
-      return await response.arrayBuffer();
-    }
-  }
-
-  throw new Error("failed to load font data");
-}
 
 export default async function Icon() {
   const content = "L";
--- app/og/route.tsx
+++ app/og/route.tsx
@@ -1,15 +1,18 @@
-import { ImageResponse } from 'next/og'
+import { ImageResponse } from "next/og";
+import { loadGoogleFont } from "./load-google-font";
+import ldsLogo from "@/app/assets/lds-logo.png";
 
-export function GET(request: Request) {
-  let url = new URL(request.url)
-  let title = url.searchParams.get('title') || 'Next.js Portfolio Starter'
+export async function GET(request: Request) {
+  let url = new URL(request.url);
+  let title = url.searchParams.get("title") || "Longhorn Design Studio";
 
   return new ImageResponse(
     (
-      <div tw="flex flex-col w-full h-full items-center justify-center bg-white">
-        <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
-          <h2 tw="flex flex-col text-4xl font-bold tracking-tight text-left">
-            {title}
+      <div tw="flex flex-col w-full h-full items-center justify-center bg-stone-950 text-marzipan-200 px-8">
+        <img src={ldsLogo.src} height={140} />
+        <div tw="flex flex-col w-full pt-12 md:items-center justify-between max-w-1/2">
+          <h2 tw="flex flex-col text-4xl tracking-tight">
+            It Could Be Time To Rethink Your Sticky Header
           </h2>
         </div>
       </div>
@@ -17,6 +20,14 @@ export function GET(request: Request) {
     {
       width: 1200,
       height: 630,
+      fonts: [
+        {
+          name: "Lora",
+          data: await loadGoogleFont("Lora", title),
+          weight: 700,
+          style: "normal",
+        },
+      ],
     }
-  )
+  );
 }
